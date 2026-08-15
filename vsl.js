290c883dd217f902fca396dacd7c7c6b70e3da1c
extremejrcanal-dot <extremejrcanal@gmail.com>
2026-08-15 00:06:53 -0300

    Update alvos function to include additional selectors

--- vsl.js
+++ vsl.js
@@ -59,7 +59,7 @@
 
   /* ---------- 2. o CTA atrasado ---------- */
   function alvos() {
-    return document.querySelectorAll(".cta-vsl, .cta-vsl-sub");
+    return document.querySelectorAll(".cta, .cta-sub, .cta-vsl, .cta-vsl-sub, .barra");
   }
   function esconder() {
     var e = alvos();
